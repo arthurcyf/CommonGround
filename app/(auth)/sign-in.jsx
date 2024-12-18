9b6442d59c534e557021aee3769bc1cf5ee09c45
btbrandon <tzhbrandon@gmail.com>
2024-12-18 21:09:30 +0800

    Key Changes
    - Refactor index.jsx to new UI
    - Add CustomButton and FormField components
    - Add sign-in page

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,11 +1,50 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { ScrollView, Text, View } from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../../constants";
+import { Image } from "react-native";
+import FormField from "../../components/FormField";
 
 const SignIn = () => {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <SafeAreaView className="bg-background h-full">
+      <ScrollView>
+        <View className="w-full justify-center h-full px-4 my-6">
+          <View className="flex-row items-center">
+            <Image
+              source={images.transparent_splash}
+              className="w-[80px] h-[80px]"
+              resizeMode="contain"
+            />
+
+            <Text className="text-4xl text-white">CommonGround</Text>
+          </View>
+          <Text className="text-2xl text-white text-semibold mt-10 font-psemibold">
+            Log in to Aora
+          </Text>
+
+          <FormField
+            title="Email"
+            value={form.email}
+            handleChangeText={(e) => setForm({ ...form, email: e })}
+            otherStyles="mt-7"
+            keyboardType="email-address"
+          />
+
+          <FormField
+            title="Password"
+            value={form.password}
+            handleChangeText={(e) => setForm({ ...form, email: e })}
+            otherStyles="mt-7"
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,16 @@
 import { Image, ScrollView } from "react-native";
-import { StatusBar, Text, View } from "react-native";
+import { Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "../constants";
 import CustomButton from "../components/CustomButton";
+import { Redirect, router } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
 export default function App() {
   return (
     <SafeAreaView className="bg-background h-full">
       <ScrollView contentContainerStyle={{ height: "100%" }}>
-        <View className="w-full justify-center items-center h-full px-4">
+        <View className="w-full justify-center items-center min-h-[85vh] px-4">
           <View className="flex-row items-center">
             <Image
               source={images.transparent_splash}
@@ -18,6 +20,7 @@ export default function App() {
 
             <Text className="text-4xl text-white">CommonGround</Text>
           </View>
+
           <View className="relative mt-5">
             <Text className="text-2xl text-white font-bold text-center">
               Discover Endless Possibilities with
@@ -33,7 +36,7 @@ export default function App() {
 
           <CustomButton
             title="Continue with Email"
-            handlePress={() => {}}
+            handlePress={() => router.push("/sign-in")}
             containerStyles="w-full mt-7"
           />
         </View>
